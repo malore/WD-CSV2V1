fe7b74c02458bf8c9cbf0bcef135c22be3ced918
malore <0malore6@gmail.com>
2018-08-11 02:07:24 +0200

    Update csv2v1.js

--- csv2v1.js
+++ csv2v1.js
@@ -1,4 +1,4 @@
-function csv2v1() {
+function csv2v1(){
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   /* store the first row */
   var data = sheet.getDataRange().getValues();
@@ -16,12 +16,31 @@ function csv2v1() {
   /* fill the new sheet */
   var k = 1;
   for(i=1;i<data.length;i++) {
-    if(data[i][0] == '') {
+    if(data[i][0] == '') { // if it's creating a new item
       newSheet.getRange(parseFloat(k),1).setValue("CREATE");
       k++;
-      for(j=0; j<data[0].length; j++) {
+      for(j=1; j<data[0].length; j++) {
         if(data[i][j] != '') {
           newSheet.getRange(parseFloat(k), 1, 1, 3).setValues([["LAST",data[0][j],data[i][j]]]);
+          k++;
+        }
+      }
+    } else { // if the item is already existent
+      for(j=1; j<data[0].length; j++) {
+        var ref = 0;
+        if(data[i][j] != '') {
+          if(data[i][j].startswith("S") && ref=0 || data[i][j].startswith("qal")) { // if reference or qualifier
+            ref = -1; // no more references are allowed in this row
+            var m = 1;
+            while(newSheet.getRange(parseFloat(k)-1, m).getValue() != ''){
+                  m++;
+            }
+            newSheet.getRange(parseFloat(k)-1, m, 1, 2).setValues([[data[0][j],data[i][j]]]);
+          }
+          if(data[i][j].startswith("s")){ // if same reference
+          
+          }
+          newSheet.getRange(parseFloat(k), 1, 1, 3).setValues([[data[i][0],data[0][j],data[i][j]]]);
           k++;
         }
       }
